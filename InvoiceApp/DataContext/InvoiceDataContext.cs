21260d8d640cdd2cbd068d2bfb3b669b0393d44e
Nemes1sX <kazstonk@gmail.com>
2023-03-08 20:48:54 +0200

    Add data context class

---
+++ InvoiceApp/DataContext/InvoiceDataContext.cs
@@ -0,0 +1,24 @@
+﻿using InvoiceApp.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace InvoiceApp.DataContext
+{
+    public class InvoiceDataContext : DbContext 
+    {
+        public InvoiceDataContext(DbContextOptions options) : base(options) 
+        { 
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<Order> Orders { get; set; }    
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<Individual> Individuals { get; set; }
+        public DbSet<LegalPerson> LegalPersons { get; set;}
+    }
+}
