011a49739394209918bf36526e1f4b0fd3e8a974
Nemes1sX <kazstonk@gmail.com>
2023-03-13 00:51:45 +0200

    Edit invoice item request class

--- InvoiceApp/Models/FormRequests/InvoiceItemRequest.cs
+++ InvoiceApp/Models/FormRequests/InvoiceItemRequest.cs
@@ -4,6 +4,8 @@ namespace InvoiceApp.Models.FormRequests
 {
     public class InvoiceItemRequest
     {
+        [Required, StringLength(255, MinimumLength = 3, ErrorMessage = "The field first name must be a string with a minimum length of 3 and a maximum length of 255.\r\n")]
+        public string Name { get; set; }
         [Required, Range(1, 100, ErrorMessage = "Quantity must be between 1 and 100")]
         public int Quantity { get; set; }
         [Required, Range(0, Double.MaxValue)]
