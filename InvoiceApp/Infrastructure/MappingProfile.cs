fc213a21547351504a5880402147a60974f3dc58
Nemes1sX <kazstonk@gmail.com>
2023-03-12 13:56:30 +0200

    Added automapper and mapping profiles

---
+++ InvoiceApp/Infrastructure/MappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using InvoiceApp.Models.Entities;
+using InvoiceApp.Models.Responses;
+
+namespace InvoiceApp.Infrastructure
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile() 
+        {
+            CreateMap<CountryResponse, Country>()
+                .ForMember(dest => dest.Name, act => act.MapFrom(src => src.name))
+                .ForMember(dest => dest.Code, act => act.MapFrom(src => src.alpha2Code))
+                .ForMember(dest => dest.Individuals, opt => opt.Ignore())
+                .ForMember(dest => dest.LegalPersons, opt => opt.Ignore())
+                .ForMember(dest => dest.VATPrecent, opt => opt.Ignore());
+        }
+    }
+}
